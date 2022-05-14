4d85c18122e514d505adfa959236be4a0c9c6e1f
ahammad <ahammadbhasha103@gmail.com>
2022-05-14 12:08:17 +0530

    added program

---
+++ program/src/inheritance/Tenth.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Tenth {
+     static int i=30;
+            int j=20;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Tenth obj=new Tenth();
+            System.out.println(Tenth.i+obj.j);
+	}
+
+}
